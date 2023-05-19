441e9dcf9ef170255908c81ac592f15cbf544cfc
Abhishek75-00 <130983558+Abhishek75-00@users.noreply.github.com>
2023-05-19 12:12:03 +0530

    Create POM

---
+++ all programs/src/all_programs/Practice.java
@@ -0,0 +1,38 @@
+package all_programs;
+
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//  int i =0;
+//  int num=0;
+//  String pri=" ";
+//  for(i=1;i<=100;i++) {
+//	  int count=0;
+//	  for(num=i;num>=1;num--) {
+//		  if(i%num==0) {
+//			  count=count+1;}}
+//	  if(count==2) {
+//		  pri=pri+ i + " ";}}
+//  System.out.println("prime no 1 to 100");
+//  System.out.println(pri);
+//	  }
+//}
+
+int i=0;
+int num=0;
+String pri=" ";
+for(i=1;i<=100;i++) {
+	int count=0;
+	for(num=i;num>=1;num--) {
+		if(i%num==0) {
+			count=count+1;}}
+	if(count==2) {
+		pri=pri+ i + " ";}}
+	System.out.println(pri);}
+	}
+	
+
+
